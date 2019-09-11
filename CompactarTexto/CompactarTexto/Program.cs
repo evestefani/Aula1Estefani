6637009c5656bdfddc2db4ba4f1b641b4538e33f
Estefani Voigt <estefani.voigt@gmail.com>
2019-09-11 15:11:56 -0300

    Commit CompactarTexto

---
+++ CompactarTexto/CompactarTexto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompactarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeiroTexto = "Hi we can do whatever we want, cool!";
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"Texto contem: {primeiroTexto.Length} caracteres.");
+
+            primeiroTexto = primeiroTexto
+                .Replace("Hi", "1").Replace("we", "2").Replace("can", "3");
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"Texto contem: {primeiroTexto.Length} caracteres.");
+
+            primeiroTexto = primeiroTexto
+                .Replace("1", "Hi").Replace("2", "we").Replace("3", "can").Replace("", "");
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"Texto contem: {primeiroTexto.Length} caracteres.");
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+
+
+}
