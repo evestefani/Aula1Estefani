ff5db1ebdd584f19aa37f52e626f40c379ee678e
Estefani Voigt <estefani.voigt@gmail.com>
2019-09-11 17:15:56 -0300

    Commit StringTypes

---
+++ StringTypesContact/StringTypesContact/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTypesContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var teste = @"alguma coisa mais alguma informação aqui";
+
+            //var teste1 = Console.ReadLine();
+            //var teste2 = Console.ReadLine();
+            ////quebra de linha
+            //var teste3 = teste1 + "\r\n" + teste2;
+
+            //Console.WriteLine(teste3);
+
+            var testeTemplate = @"Nome do usuario:{0}
+Idade:{1}
+Descrição:{2}
+Hummmm:{0}";
+
+            var finishText = string.Format(testeTemplate
+                , Console.ReadLine()
+                , Console.ReadLine()
+                , Console.ReadLine());
+
+            //Console.WriteLine(finishText);
+
+            Console.WriteLine(finishText);
+            Console.ReadKey();
+
+
+        }
+    }
+}
