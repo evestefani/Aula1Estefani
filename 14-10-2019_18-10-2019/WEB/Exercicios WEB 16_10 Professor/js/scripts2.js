97e29542892dfc7e4fc1ec38532247066c9354cd
Estefani Voigt <estefani.voigt@gmail.com>
2019-10-18 10:25:07 -0300

    Commit Atualização dos Exercicios - Mask

--- 14-10-2019_18-10-2019/WEB/Exercicios WEB 16_10 Professor/js/scripts2.js
+++ 14-10-2019_18-10-2019/WEB/Exercicios WEB 16_10 Professor/js/scripts2.js
@@ -3,21 +3,26 @@ $(document).ready(
     function () {
         $('input[name=btnconverter]').click(function () {
 
-            var valordolar = 4.16;
-            var valoreal = $('input[name="valoreal"]').val().replace(".","").replace(",",".");
+            var url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2710-16-2019%27&$top=100&$format=json"
 
-            var convercao = (valoreal / valordolar)
-                .toFixed(2)
-                .toString()
-                .replace(/(\d)(\d{2})$/, "$1,$2");
+            $.getJSON(url, function (data) {
 
-            $('input[name="valordolar"]').val(convercao);
+                var valordolar = data.value[0].cotacaoVenda;
+                /*Obter um valor*/
+                var valoreal = $('input[name="valoreal"]').val().replace(".", "").replace(",", ".");
+
+                var convercao = (valoreal / valordolar)
+                    .toLocaleString("en-US", { minimumFractionDigits: 2, currency: "USD" });
+
+                /*Inserir um valor*/
+                $('input[name="valordolar"]').val(convercao);
+
+             });
 
         });
 
-        $('input[name="valoreal"]').mask("000.000,00");
+        $('input[name="valoreal"]').maskMoney({ showSymbol: false, symbol: "R$", decimal: ",", thousands: "." });
 
     }
 
 );
-
