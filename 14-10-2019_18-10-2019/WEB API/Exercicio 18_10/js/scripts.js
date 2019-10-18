ee412122df0b697f27e6679e3f8dc8a423d673ae
Estefani Voigt <estefani.voigt@gmail.com>
2019-10-18 11:56:00 -0300

    Commit - Exercicio API 18_10

---
+++ 14-10-2019_18-10-2019/WEB API/Exercicio 18_10/js/scripts.js
@@ -0,0 +1,28 @@
+﻿var meuPet = { nome: "Lou", idade: "3 meses", raca: "SRD", coloracao: "Tigrada" };
+
+$(document).ready(
+    function () {
+        
+        $('input[name="nome"]').val(meuPet.nome);
+        $('input[name="idade"]').val(meuPet.idade);
+        $('input[name="raca"]').val(meuPet.raca);
+        $('input[name="coloracao"]').val(meuPet.coloracao);
+        
+
+        $('input[name="btnsalvar"]').click(function () {
+        var nome = meuPet.nome = $('input[name="nome"]').val();
+        var idade = meuPet.idade = $('input[name="idade"]').val();
+        var raca = meuPet.raca = $('input[name="raca"]').val();
+        var coloracao = meuPet.coloracao = $('input[name="coloracao"]').val();
+
+        alert("Nome do pet: " + nome + "\nIdade do pet: " + idade + "\nRaça do pet: " + raca + "\nColoração do pet: " + coloracao);
+
+        });
+
+        $('input[name="btnmostrar"]').click(function () {
+            alert("Nome do pet: " + nome + "\nIdade do pet: " + idade + "\nRaça do pet: " + raca + "\nColoração do pet: " + coloracao);
+        });
+
+    }
+
+);
