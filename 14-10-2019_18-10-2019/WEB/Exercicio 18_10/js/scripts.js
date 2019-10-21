aa5b2c1f87c6a1f1156e6c5b69e97e1520b7d916
Estefani Voigt <estefani.voigt@gmail.com>
2019-10-21 17:30:51 -0300

    Commit Atualização dos Exercicios 18_10

---
+++ 14-10-2019_18-10-2019/WEB/Exercicio 18_10/js/scripts.js
@@ -0,0 +1,43 @@
+﻿var minhaBike = { nome: "Lou", idade: "3 meses", raca: "SRD", coloracao: "Tigrada" };
+
+
+$(document).ready(
+    function () {
+        
+        $('input[name="nome"]').val(minhaBike.nome);
+        $('input[name="idade"]').val(minhaBike.idade);
+        $('input[name="raca"]').val(minhaBike.raca);
+        $('input[name="coloracao"]').val(minhaBike.coloracao);
+        
+
+        $('input[name="btnsalvar"]').click(function () {
+        var nome = minhaBike.nome = $('input[name="nome"]').val();
+        var idade = minhaBike.idade = $('input[name="idade"]').val();
+        var raca = minhaBike.raca = $('input[name="raca"]').val();
+        var coloracao = minhaBike.coloracao = $('input[name="coloracao"]').val();
+
+      
+
+        alert("Nome do pet: " + nome + "\n Idade do pet: " + idade + "\nRaça do pet: " + raca + "\nColoração do pet: " + coloracao);
+
+            CleanScreen();
+
+        });
+
+        $('input[name="btnmostrar"]').click(function ()
+        {
+            alert("Nome do pet: " + minhaBike.nome + "\n Idade do pet: " + minhaBike.idade + "\nRaça do pet: " + minhaBike.raca + "\nColoração do pet: " + minhaBike.coloracao);
+        });
+
+    }
+
+);
+
+function CleanScreen() {
+    var meusInputs = $('input[type="text"]');
+
+    for (var i = 0; i < meusInputs.length; i++) {
+        meusInputs.val("");
+
+    }
+}
---
+++ 14-10-2019_18-10-2019/WEB/Exercicio 18_10/js/scripts2.js
@@ -0,0 +1,40 @@
+﻿var minhaBike = { marca: "Monarque", valor: "R$ 800,00", peso: "10kg" };
+
+
+$(document).ready(
+    function () {
+        
+        $('input[name="marca"]').val(minhaBike.marca);
+        $('input[name="valor"]').val(minhaBike.valor);
+        $('input[name="peso"]').val(minhaBike.peso);
+
+        
+
+        $('input[name="btnsalvar"]').click(function () {
+        var marca = minhaBike.marca = $('input[name="marca"]').val();
+        var valor = minhaBike.valor = $('input[name="valor"]').val();
+        var peso = minhaBike.peso = $('input[name="peso"]').val();
+
+        alert("Marca da bike: " + marca + "\n Valor da bike : " + valor + "\nPeso da bike: " + peso);
+
+            CleanScreen();
+
+        });
+
+        $('input[name="btnmostrar"]').click(function ()
+        {
+            alert("Marca da bike: " + minhaBike.marca + "\n Valor da bike : " + minhaBike.valor + "\nPeso da bike: " + minhaBike.peso);
+        });
+
+    }
+
+);
+
+function CleanScreen() {
+    var meusInputs = $('input[type="text"]');
+
+    for (var i = 0; i < meusInputs.length; i++) {
+        meusInputs.val("");
+
+    }
+}
