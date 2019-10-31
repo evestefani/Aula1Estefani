f0414237dbdb0a35dbbd377edd5b935ea556b5a2
Estefani Voigt <estefani.voigt@gmail.com>
2019-10-31 17:19:37 -0300

    Commit - Atualização  31-10

---
+++ RegistroDeImoveis/RegistroDeImoveis/Migrations/201910311437025_AlteracoesCustomModelConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace RegistroDeImoveis.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlteracoesCustomModelConfiguration : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Imovels", "Numero", c => c.String());
+            AlterColumn("dbo.Imovels", "Cep", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Imovels", "Cep", c => c.Int(nullable: false));
+            AlterColumn("dbo.Imovels", "Numero", c => c.Int(nullable: false));
+        }
+    }
+}
--- RegistroDeImoveis/RegistroDeImoveis/Models/CustomValidator.cs
+++ RegistroDeImoveis/RegistroDeImoveis/Models/CustomValidator.cs
@@ -8,13 +8,43 @@ namespace RegistroDeImoveis.Models
 {
     public class CustomValidator : ValidationAttribute
     {
+        RegistroDeImoveisContext db = new RegistroDeImoveisContext();
+
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            RegistroDeImoveisContext db = new RegistroDeImoveisContext();
+            if (validationContext.DisplayName == "Nome")
+            {
+                if (db.Proprietarios
+                    .FirstOrDefault(x => x.Nome == value.ToString()) != null)
+                    return new ValidationResult("Este usuário já existe");
 
-            if (db.Proprietarios.FirstOrDefault(x => x.Nome == value.ToString()) != null)
-                return new ValidationResult("Usuário já cadastrado");
-            //throw new Exception("Usuário já existe na nossa base de dados");
+                if (value.ToString().Contains("Estefani"))
+                    return new ValidationResult("Não é possivel registar esse nome em nossa base de dados porque ele é unico.");
+            }
+
+            if (validationContext.DisplayName == "Email")
+            {
+                if (db.Proprietarios
+                    .FirstOrDefault(x => x.Email == value.ToString()) != null)
+                    return new ValidationResult("Este email já existe");
+
+                if (value.ToString().Contains("Estefani"))
+                    return new ValidationResult("Não é possivel registar esse email em nossa base de dados porque ele é unico.");
+            }
+
+            if (validationContext.DisplayName == "DataDeNascimento")
+            {
+
+                var dataInformada = DateTime.Parse(value.ToString());
+                var idade = DateTime.Now.Year - dataInformada.Year;
+
+                if (idade < 18)
+                    return new ValidationResult("É preciso ser maior de 18 anos para efetuar o cadastro!");
+
+                if (idade > 150)
+                    return new ValidationResult("Idade apresentada é maior que o permitido!");
+
+            }
 
             return ValidationResult.Success;
         }
