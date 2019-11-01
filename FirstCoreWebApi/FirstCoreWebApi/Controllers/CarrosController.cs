13b691af36ebbd287f2e11115253daa09f32474a
Estefani Voigt <estefani.voigt@gmail.com>
2019-11-01 17:28:39 -0300

    Commit  - Iniciando Core + WebApi

---
+++ FirstCoreWebApi/FirstCoreWebApi/Controllers/CarrosController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FirstCoreWebApi.Model;
+
+namespace FirstCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrosController : ControllerBase
+    {
+        private readonly FirstCoreContext _context;
+
+        public CarrosController(FirstCoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Carros
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Carro>>> GetCarros()
+        {
+            return await _context.Carros.ToListAsync();
+        }
+
+        // GET: api/Carros/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Carro>> GetCarro(int id)
+        {
+            var carro = await _context.Carros.FindAsync(id);
+
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            return carro;
+        }
+
+        // PUT: api/Carros/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarro(int id, Carro carro)
+        {
+            if (id != carro.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carro).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Carros
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Carro>> PostCarro(Carro carro)
+        {
+            _context.Carros.Add(carro);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCarro), new { id = carro.Id }, carro);
+        }
+
+        // DELETE: api/Carros/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Carro>> DeleteCarro(int id)
+        {
+            var carro = await _context.Carros.FindAsync(id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carros.Remove(carro);
+            await _context.SaveChangesAsync();
+
+            return carro;
+        }
+
+        private bool CarroExists(int id)
+        {
+            return _context.Carros.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FirstCoreWebApi/FirstCoreWebApi/Model/FirstCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FirstCoreWebApi.Model
+{
+    public class FirstCoreContext : DbContext
+    {
+        public FirstCoreContext(DbContextOptions<FirstCoreContext> options)
+            : base(options)
+        { 
+        }
+        public DbSet<Carro> Carros { get; set; }    
+    }
+}
