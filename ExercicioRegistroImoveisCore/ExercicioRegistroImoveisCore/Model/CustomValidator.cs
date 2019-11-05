1bb2f67eab3e775b00a2a7e847782a1fd4cb2d01
Estefani Voigt <estefani.voigt@gmail.com>
2019-11-05 16:31:44 -0300

    Commit  - Atualização

--- ExercicioRegistroImoveisCore/ExercicioRegistroImoveisCore/Model/CustomValidator.cs
+++ ExercicioRegistroImoveisCore/ExercicioRegistroImoveisCore/Model/CustomValidator.cs
@@ -34,20 +34,7 @@ namespace ExercicioRegistroImoveisCore.Model
                     return new ValidationResult("Não é possivel registar esse email em nossa base de dados porque ele é unico.");
             }
 
-            if (validationContext.DisplayName == "DataDeNascimento")
-            {
-
-                var dataInformada = DateTime.Parse(value.ToString());
-                var idade = DateTime.Now.Year - dataInformada.Year;
-
-                if (idade < 18)
-                    return new ValidationResult("É preciso ser maior de 18 anos para efetuar o cadastro!");
-
-                if (idade > 150)
-                    return new ValidationResult("Idade apresentada é maior que o permitido!");
-
-            }
-
+            
             return ValidationResult.Success;
         }
     }
